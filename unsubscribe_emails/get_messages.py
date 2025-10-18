210ba26799567a76098a8b44ad568f360f5875b1
gxelol <gabrielquintaog@gmail.com>
2025-10-17 23:51:36 -0300

    test: code to test decode function

--- unsubscribe_emails/get_messages.py
+++ unsubscribe_emails/get_messages.py
@@ -1,10 +1,10 @@
 import requests
-import os
-import base64
 
+from decode import decode_messages   
 from auth import authenticate_gmail
 
 def list_messages():
+    # Authenticate and get access token
     creds = authenticate_gmail()
     access_token = creds.token
     url = 'https://gmail.googleapis.com/gmail/v1/users/{userId}/messages'
@@ -12,19 +12,34 @@ def list_messages():
         'Authorization': f'Bearer {access_token}',
     }
 
+    # Get the list of messages
     res_messages = requests.get(url.format(userId='me'), headers=headers)
-
+    
+    # Extract message IDs from the response
     messages = res_messages.json().get("messages", [])
 
     messages_ids = [msg['id'] for msg in messages]
 
-    first_1_id = messages_ids[:1]
+    message = messages_ids[:1]
     
-    for msg_id in first_1_id:
-        res_message = requests.get(f'https://gmail.googleapis.com/gmail/v1/users/me/messages/{msg_id}', headers=headers)
+    res_message = requests.get(f'https://gmail.googleapis.com/gmail/v1/users/me/messages/{message[0]}', headers=headers)
 
-        print(f'Messages: {res_message.json()}')
-    
+    res_message_json = res_message.json()
+
+    # Extract the message payload and decode the body
+    payload = res_message_json.get("payload", {})
+    parts = payload.get("parts", [])
+
+    # Iterate through parts to find and decode the message body
+    for part in parts:
+        body = part.get("body", {})
+        data = body.get("data", "")
+        encoding = body.get("size", 0)
+
+        # Get content transfer encoding
+        if data and encoding:
+            decoded_body = decode_messages(data, content_transfer_encoding=encoding)
+            print(f'Decoded message body: {decoded_body}')
 
 if __name__ == "__main__":
     list_messages()
