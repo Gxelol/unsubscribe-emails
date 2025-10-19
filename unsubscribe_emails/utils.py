5c8459da62801f777a12f4f5856d5942834321a5
gxelol <gabrielquintaog@gmail.com>
2025-10-19 18:32:08 -0300

    feat: check url with google safe browsing API

--- unsubscribe_emails/utils.py
+++ unsubscribe_emails/utils.py
@@ -1,34 +1,82 @@
 import base64
 import quopri
-from urllib.parse import urlparse
 import requests
+import json
+import logging
 
-def decode_messages(data_base64: str, content_transfer_encoding: str = "base64") -> str:
-    # Decode the message body based on the content transfer encoding
-    if content_transfer_encoding == "quoted-printable":
+from urllib.parse import urlparse
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# def decode_messages(data_base64: str, content_transfer_encoding: str = "base64") -> str:
+#     # Decode the message body based on the content transfer encoding
+#     if content_transfer_encoding == "quoted-printable":
         
-        # Decode quoted-printable
-        quoted_decoded = quopri.decodestring(data_base64)
-        return quoted_decoded.decode("utf-8", errors="replace")
+#         # Decode quoted-printable
+#         quoted_decoded = quopri.decodestring(data_base64)
+#         return quoted_decoded.decode("utf-8", errors="replace")
 
-    # Base64 decoding
-    base64_bytes = data_base64.encode("utf-8")
-    decoded_bytes = base64.urlsafe_b64decode(base64_bytes + b'==')
-    return decoded_bytes.decode("utf-8", errors="replace")
+#     # Base64 decoding
+#     base64_bytes = data_base64.encode("utf-8")
+#     decoded_bytes = base64.urlsafe_b64decode(base64_bytes + b'==')
+#     return decoded_bytes.decode("utf-8", errors="replace")
 
-def check_url(url: str) -> dict:
+def check_url(url: str, api_key: str) -> dict:
     if url.startswith("<") and url.endswith(">"):
         url = url[1:-1]
     
     if url.startswith("http://"):
-        return {"error": "Insecure URL (http)"}
+        return False
     elif url.startswith("https://"):
-        extract_domain(url)
-        return {"success": "Secure URL, the token was extracted"}
+        url_without_token = withdraw_token(url)
+        return check_domain(url_without_token, api_key)
     else:
-        return {"error": "Invalid URL format"}
+        return {"error": "URL not suported"}
 
-def extract_domain(url: str) -> str:
+def withdraw_token(url: str) -> str:
     url_parts = urlparse(url)
     domain = url_parts.netloc
-    return domain
+    https = url_parts.scheme
+    new_url = https + '://' + domain
+    return new_url
+
+def check_domain(urls: list, api_key: str) -> bool:
+    url = f"https://safebrowsing.googleapis.com/v4/threatMatches:find?key={api_key}"
+
+    headers = {
+    'Content-Type': 'application/json',
+    }
+
+
+    body =   {
+        "client": {
+        "clientId": "Nothing",
+        "clientVersion": "1.5.2"
+        },
+        "threatInfo": {
+        "threatTypes": [
+            "MALWARE", 
+            "SOCIAL_ENGINEERING", 
+            "UNWANTED_SOFTWARE", 
+            "POTENTIALLY_HARMFUL_APPLICATION"
+        ],
+        "platformTypes":    ["WINDOWS", "LINUX", "ANDROID"],
+        "threatEntryTypes": ["URL", "EXECUTABLE"],
+        "threatEntries": [
+            {"url": urls},
+        ]
+        }
+    }
+
+    response = requests.post(url, headers=headers, data=json.dumps(body))
+
+    if response.status_code == 200:
+        result = response.json()
+        if 'matches' in result:
+            logging.warning(f"The URL {urls} is listed as dangerous.")
+            return False
+        else:
+            logging.info(f"The URL {urls} is safe.")
+            return True
+    else:
+        return {"error": f"Error: {response.status_code}, Message: {response.text}"}
